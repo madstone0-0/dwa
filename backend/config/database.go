fb56aa243e87e81e434bb7c6b31eab73af5ff693
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-02-09 14:18:20 +0000

    CU-8697rjt9u - Integration test

--- backend/config/database.go
+++ backend/config/database.go
@@ -23,7 +23,6 @@ func (d *Database) DSN() (string, error) {
 		return "", nil
 	}
 
-	// 	user=jack password=secret host=pg.example.com port=5432 dbname=mydb sslmode=verify-ca pool_max_conns=10 pool_max_conn_lifetime=1h30m
 	return fmt.Sprintf(
 		"user=%s password=%s host=%s port=%s dbname=%s",
 		d.Username,
