a4e0357422ff1fb148be3cf7be43ea142ce63ec6
nignig0 <tanitoluwaadebayo@gmail.com>
2025-04-03 08:28:44 +0000

    working on the endpoint to get by item id

--- backend/routes/auth/item/item.go
+++ backend/routes/auth/item/item.go
@@ -1,11 +1,14 @@
 package item
 
 import (
+	"backend/internal/logging"
+	"backend/internal/utils"
 	"backend/repository"
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -16,9 +19,28 @@ func ItemRoutes(ctx context.Context, pool *pgxpool.Pool, rg *gin.RouterGroup) {
 		//get the items by the vendor id and then return them
 		q := repository.New(pool)
 		vId := c.Param("vId")
+		var id pgtype.UUID
+		err := id.Scan(vId)
 
-		id, err := uuid.FromString(vId)
+		if err != nil {
+			logging.Errorf("Error parsing vId to uuid -> %v", err)
+			utils.SendErr(c, http.StatusInternalServerError, err)
+			return
+		}
 
-		items, error := q.GetItemsByVendorId(ctx, vId)
+		items, err := q.GetItemsByVendorId(ctx, id)
+
+		if err != nil {
+			utils.SendErr(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		utils.SendData(c, http.StatusOK, items)
+
+	})
+
+	item.GET("/:itemId", func(c *gin.Context){
+		q := repository.New(pool)
+		itemId := c.Param("itemId")
 	})
 }
