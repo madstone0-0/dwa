d75868b4c4188ae133b3910242ba95aac7b27a37
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-04-07 13:03:00 +0000

    CU-8697w1gr9, CU-8698j47g0 - Add email verification

--- backend/services/auth/auth_test.go
+++ backend/services/auth/auth_test.go
@@ -223,7 +223,7 @@ func TestSignUp(t *testing.T) {
 			Password: "password",
 			Name:     "Test Buyer",
 			IsVendor: false,
-		})
+		}, false)
 
 		if result.ServiceErr != nil {
 			t.Logf("Result: %+v", result)
@@ -245,7 +245,7 @@ func TestSignUp(t *testing.T) {
 			Password: "password",
 			Name:     "Existing User",
 			IsVendor: false,
-		})
+		}, false)
 
 		if result.ServiceErr != nil {
 			t.Logf("Result: %+v", result)
