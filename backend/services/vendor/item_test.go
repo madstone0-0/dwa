b45a32c26755c5cb00f1562131f5c8c29ff7b5ef
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-04-10 11:40:49 +0000

    CU-8698mywq9 - Finish item_test

--- backend/services/vendor/item_test.go
+++ backend/services/vendor/item_test.go
@@ -11,24 +11,39 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v5"
-	_ "github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
+// vendorScanExists is a helper function to setup a mock row that confirms a vendor exists on scan
 func vendorScanExists(mockRow *it.MockRow) {
 	it.SetupScanReturnArgs(mockRow, nil, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything)
 }
+
+// itemScanExists is a helper function to setup a mock row that confirms an item exists on scan
 func itemScanExists(mockRow *it.MockRow) {
 	it.SetupScanReturnArgs(mockRow, nil, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything)
 }
 
+// vendorScanNotExists  is a helper function to setup a mock row that confirms a vendor does exists on scan returning an
+// appropriate error
 func vendorScanNotExists(mockRow *it.MockRow, err error) {
 	it.SetupScanReturnArgs(mockRow, err, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything)
 }
+
+// itemScanNotExists is a helper function to setup a mock row that confirms an item does exists on scan returning an
+// appropriate error
 func itemScanNotExists(mockRow *it.MockRow, err error) {
 	it.SetupScanReturnArgs(mockRow, err, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+}
+
+// printError is a helper function to print out unexpected errors
+func printError(err error, t *testing.T) {
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
 }
 
 func TestDoesVendorExistById(t *testing.T) {
@@ -43,6 +58,8 @@ func TestDoesVendorExistById(t *testing.T) {
 
 		exists, err := doesVendorExistById(ctx, &mockPool, testUUID)
 
+		printError(err, t)
+
 		assert.NoError(t, err)
 		assert.True(t, exists)
 
@@ -56,6 +73,8 @@ func TestDoesVendorExistById(t *testing.T) {
 		it.SetupPoolQueryRow(&mockPool, mockRow, repository.GetVendorById, ctx, []any{testUUID})
 
 		exists, err := doesVendorExistById(ctx, &mockPool, testUUID)
+
+		printError(err, t)
 
 		assert.NoError(t, err)
 		assert.False(t, exists)
@@ -92,6 +111,8 @@ func TestDoesItemExistByName(t *testing.T) {
 
 		exists, err := doesItemExistByName(ctx, &mockPool, testName)
 
+		printError(err, t)
+
 		assert.NoError(t, err)
 		assert.True(t, exists)
 
@@ -105,6 +126,8 @@ func TestDoesItemExistByName(t *testing.T) {
 		it.SetupPoolQueryRow(&mockPool, mockRow, repository.GetItemByName, ctx, []any{testName})
 
 		exists, err := doesItemExistByName(ctx, &mockPool, testName)
+
+		printError(err, t)
 
 		assert.NoError(t, err)
 		assert.False(t, exists)
@@ -189,11 +212,13 @@ func TestAll(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		mockRow := &it.MockRow{}
 		mockRows := &it.MockRows{}
+
+		// Sample item and Vid for testing
 		testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
 		testItem := repository.Item{
 			Iid:         pgtype.UUID{Bytes: [16]byte{1}, Valid: true},
 			Vid:         testVid,
-			Name:        "Schrodingers Cat",
+			Name:        "Schrodingers Cat 1",
 			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
 			Description: utils.MakePointer("Probably dead"),
 			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
@@ -205,6 +230,7 @@ func TestAll(t *testing.T) {
 		it.SetupMock(mockRows, "Next", []any{}, true).Once()
 		it.SetupMock(mockRows, "Next", []any{}, false).Once()
 		it.SetupMock(mockRows, "Err", []any{}, nil)
+		// Assign values to the pointers passed to Scan when a select operation is performed
 		it.SetupMock(mockRows, "Scan", []any{mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything}, nil).Run(
 			func(args mock.Arguments) {
 				if dest, ok := args.Get(0).(*pgtype.UUID); ok {
@@ -231,9 +257,14 @@ func TestAll(t *testing.T) {
 
 		result := All(ctx, mockPool, testVid)
 
+		if result.ServiceErr != nil {
+			printError(result.ServiceErr.Err, t)
+		}
+
 		assert.Nil(t, result.ServiceErr)
 		assert.Equal(t, http.StatusOK, result.Status)
 		items := result.Data.(utils.JMap)["items"].([]repository.Item)
+		// Check that all the values are correct
 		assert.Equal(t, 1, len(items))
 		assert.Equal(t, testItem.Iid, items[0].Iid)
 		assert.Equal(t, testItem.Vid, items[0].Vid)
@@ -241,6 +272,7 @@ func TestAll(t *testing.T) {
 		assert.Equal(t, *testItem.Pictureurl, *items[0].Pictureurl)
 		assert.Equal(t, *testItem.Description, *items[0].Description)
 		assert.Equal(t, testItem.Cost, items[0].Cost)
+
 		mockRow.AssertExpectations(t)
 		mockRows.AssertExpectations(t)
 	})
@@ -254,6 +286,7 @@ func TestAll(t *testing.T) {
 		result := All(ctx, mockPool, testVid)
 
 		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
 		assert.Equal(t, http.StatusNotFound, result.ServiceErr.Status)
 		mockRow.AssertExpectations(t)
 	})
@@ -268,9 +301,443 @@ func TestAll(t *testing.T) {
 
 		result := All(ctx, mockPool, testVid)
 
+		assert.NotNil(t, result.ServiceErr)
 		assert.Error(t, result.ServiceErr.Err)
 		assert.Equal(t, http.StatusInternalServerError, result.ServiceErr.Status)
 		mockRow.AssertExpectations(t)
+	})
+
+	mockPool.AssertExpectations(t)
+}
+
+func TestAdd(t *testing.T) {
+	ctx := context.Background()
+	mockPool := &it.MockPool{}
+
+	t.Run("Success", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 1",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.InsertItem, ctx, []any{
+			testVid,
+			testItem.Name,
+			testItem.Pictureurl,
+			testItem.Description,
+			testItem.Cost,
+		})
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemByName, ctx, []any{testItem.Name})
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			pgx.ErrNoRows)
+		// Assign test Iid to the iid pointer passed to Scan when a select operation is performed
+		it.SetupMock(itemRow, "Scan", []any{mock.AnythingOfType("*pgtype.UUID")}, nil).Run(func(args mock.Arguments) {
+			if dest, ok := args.Get(0).(*pgtype.UUID); ok {
+				*dest = testIid
+			}
+		})
+		vendorScanExists(vendorRow)
+
+		result := Add(ctx, mockPool, testItem)
+
+		if result.ServiceErr != nil {
+			printError(result.ServiceErr.Err, t)
+		}
+
+		assert.Nil(t, result.ServiceErr)
+		assert.Equal(t, http.StatusCreated, result.Status)
+		iid := result.Data.(utils.JMap)["iid"].(pgtype.UUID)
+		assert.NotNil(t, iid)
+		assert.Equal(t, testIid, iid)
+		vendorRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+	})
+
+	t.Run("Vendor not found", func(t *testing.T) {
+		mockRow := &it.MockRow{}
+		testVid := pgtype.UUID{Bytes: [16]byte{2}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 2",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+		vendorScanNotExists(mockRow, pgx.ErrNoRows)
+		it.SetupPoolQueryRow(mockPool, mockRow, repository.GetVendorById, ctx, []any{testVid})
+
+		result := Add(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusNotFound, result.ServiceErr.Status)
+		mockRow.AssertExpectations(t)
+	})
+
+	t.Run("Item already exists", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{3}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 3",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemByName, ctx, []any{testItem.Name})
+		itemScanExists(itemRow)
+		vendorScanExists(vendorRow)
+
+		result := Add(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusConflict, result.ServiceErr.Status)
+		vendorRow.AssertExpectations(t)
+		itemRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+	})
+
+	t.Run("Insert error", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{4}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 4",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.InsertItem, ctx, []any{
+			testVid,
+			testItem.Name,
+			testItem.Pictureurl,
+			testItem.Description,
+			testItem.Cost,
+		})
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemByName, ctx, []any{testItem.Name})
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			pgx.ErrNoRows)
+		it.SetupMock(itemRow, "Scan", []any{mock.AnythingOfType("*pgtype.UUID")}, errors.New("e"))
+		vendorScanExists(vendorRow)
+
+		result := Add(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusInternalServerError, result.ServiceErr.Status)
+		itemRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+	})
+
+	mockPool.AssertExpectations(t)
+}
+
+func TestUpdate(t *testing.T) {
+	ctx := context.Background()
+	mockPool := &it.MockPool{}
+
+	t.Run("Success", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testItem := repository.UpdateItemParams{
+			Iid:         testIid,
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 1",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testItem.Iid})
+		execCall := it.SetupMock(mockPool, "Exec", []any{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}, pgconn.CommandTag{}, nil)
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			nil)
+		it.SetupMock(itemRow, "Scan", []any{mock.AnythingOfType("*pgtype.UUID")}, nil).Run(func(args mock.Arguments) {
+			if dest, ok := args.Get(0).(*pgtype.UUID); ok {
+				*dest = testIid
+			}
+		})
+		vendorScanExists(vendorRow)
+
+		result := Update(ctx, mockPool, testItem)
+
+		if result.ServiceErr != nil {
+			printError(result.ServiceErr.Err, t)
+		}
+
+		assert.Nil(t, result.ServiceErr)
+		assert.Equal(t, http.StatusOK, result.Status)
+		msg := result.Data.(utils.JMap)["msg"].(string)
+		assert.NotNil(t, msg)
+		assert.Equal(t, "Item updated", msg)
+		vendorRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+		execCall.Unset()
+	})
+
+	t.Run("Vendor not found", func(t *testing.T) {
+		mockRow := &it.MockRow{}
+		testVid := pgtype.UUID{Bytes: [16]byte{2}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 2",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+		vendorScanNotExists(mockRow, pgx.ErrNoRows)
+		it.SetupPoolQueryRow(mockPool, mockRow, repository.GetVendorById, ctx, []any{testVid})
+
+		result := Add(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusNotFound, result.ServiceErr.Status)
+		mockRow.AssertExpectations(t)
+	})
+
+	t.Run("Item already exists", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{3}, Valid: true}
+		// testIid := pgtype.UUID{Bytes: [16]byte{2}, Valid: true}
+		testItem := repository.InsertItemParams{
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 3",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemByName, ctx, []any{testItem.Name})
+		itemScanExists(itemRow)
+		vendorScanExists(vendorRow)
+
+		result := Add(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusConflict, result.ServiceErr.Status)
+		vendorRow.AssertExpectations(t)
+		itemRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+	})
+
+	t.Run("Update error", func(t *testing.T) {
+		vendorRow := &it.MockRow{}
+		itemRow := &it.MockRow{}
+		mockRows := &it.MockRows{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{4}, Valid: true}
+		testIid := pgtype.UUID{Bytes: [16]byte{4}, Valid: true}
+		testItem := repository.UpdateItemParams{
+			Iid:         testIid,
+			Vid:         testVid,
+			Name:        "Schrodingers Cat 4",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100)},
+		}
+
+		it.SetupPoolQueryRow(mockPool, vendorRow, repository.GetVendorById, ctx, []any{testVid})
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testItem.Iid})
+		it.SetupMock(mockPool, "Exec", []any{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}, pgconn.CommandTag{}, errors.New("e"))
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			nil)
+		it.SetupMock(itemRow, "Scan", []any{mock.AnythingOfType("*pgtype.UUID")}, nil).Run(func(args mock.Arguments) {
+			if dest, ok := args.Get(0).(*pgtype.UUID); ok {
+				*dest = testIid
+			}
+		})
+		vendorScanExists(vendorRow)
+
+		result := Update(ctx, mockPool, testItem)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusInternalServerError, result.ServiceErr.Status)
+		vendorRow.AssertExpectations(t)
+		mockRows.AssertExpectations(t)
+	})
+
+	mockPool.AssertExpectations(t)
+}
+
+func TestDelete(t *testing.T) {
+	ctx := context.Background()
+	mockPool := &it.MockPool{}
+
+	t.Run("Success", func(t *testing.T) {
+		itemRow := &it.MockRow{}
+
+		testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testIid})
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			nil)
+		execCall := it.SetupMock(mockPool, "Exec", []any{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}, pgconn.CommandTag{}, nil)
+		itemScanExists(itemRow)
+
+		result := Delete(ctx, mockPool, testIid)
+
+		if result.ServiceErr != nil {
+			printError(result.ServiceErr.Err, t)
+		}
+
+		assert.Nil(t, result.ServiceErr)
+		assert.Equal(t, http.StatusOK, result.Status)
+		execCall.Unset()
+
+	})
+
+	t.Run("Item not found", func(t *testing.T) {
+		itemRow := &it.MockRow{}
+
+		testIid := pgtype.UUID{Bytes: [16]byte{2}, Valid: true}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testIid})
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			pgx.ErrNoRows)
+		itemScanNotExists(itemRow, pgx.ErrNoRows)
+
+		result := Delete(ctx, mockPool, testIid)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusNotFound, result.ServiceErr.Status)
+		itemRow.AssertExpectations(t)
+	})
+
+	t.Run("Delete error", func(t *testing.T) {
+		itemRow := &it.MockRow{}
+
+		testIid := pgtype.UUID{Bytes: [16]byte{3}, Valid: true}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testIid})
+		it.SetupMock(itemRow, "Scan",
+			[]any{
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*pgtype.UUID"),
+				mock.AnythingOfType("*string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("**string"),
+				mock.AnythingOfType("*pgtype.Numeric"),
+			},
+			nil)
+		it.SetupMock(mockPool, "Exec", []any{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}, pgconn.CommandTag{}, errors.New("e"))
+		itemScanExists(itemRow)
+
+		result := Delete(ctx, mockPool, testIid)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusInternalServerError, result.ServiceErr.Status)
+		itemRow.AssertExpectations(t)
 	})
 
 	mockPool.AssertExpectations(t)
