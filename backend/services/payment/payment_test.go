27e8eab79a5969a21a8f9912670c40cae4ede749
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-04-21 14:34:50 +0000

    Improve CreateTransactionRecord tests
    - Update mock pool instantiation
    - Add test for item not found scenario
    - Add test for vendor ID mismatch
    - Ensure expectations are met for all mock interactions

--- backend/services/payment/payment_test.go
+++ backend/services/payment/payment_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -16,9 +17,9 @@ import (
 
 func TestCreateTransactionRecord(t *testing.T) {
 	ctx := context.Background()
-	mockPool := &it.MockPool{}
 
 	t.Run("Success", func(t *testing.T) {
+		mockPool := &it.MockPool{}
 		itemRow := &it.MockRow{}
 
 		testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
@@ -81,19 +82,172 @@ func TestCreateTransactionRecord(t *testing.T) {
 			},
 		)
 
-		res := CreateTransactionRecord(ctx, mockPool, testTrans)
-		if res.ServiceErr != nil {
-			it.PrintError(res.ServiceErr.Err, t)
+		result := CreateTransactionRecord(ctx, mockPool, testTrans)
+		if result.ServiceErr != nil {
+			it.PrintError(result.ServiceErr.Err, t)
 		}
 
-		assert.Nil(t, res.ServiceErr)
-		assert.Equal(t, http.StatusOK, res.Status)
-		tid := res.Data.(utils.JMap)["tid"].(pgtype.UUID)
+		assert.Nil(t, result.ServiceErr)
+		assert.Equal(t, http.StatusOK, result.Status)
+		tid := result.Data.(utils.JMap)["tid"].(pgtype.UUID)
 		assert.NotNil(t, tid)
 		assert.Equal(t, testTid, tid)
+
 		itemRow.AssertExpectations(t)
+		mockPool.AssertExpectations(t)
 	})
 
-	mockPool.AssertExpectations(t)
+	t.Run("Item not found", func(t *testing.T) {
+		mockPool := &it.MockPool{}
+		itemRow := &it.MockRow{}
+
+		testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testBid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testTrans := repository.CreateTransactionParams{
+			Bid: testBid,
+			Vid: testVid,
+			Iid: testIid,
+			Amt: pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+		}
+		testItem := repository.Item{
+			Iid: testIid,
+		}
+
+		it.ItemScanNotExists(itemRow, pgx.ErrNoRows)
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testItem.Iid})
+
+		result := CreateTransactionRecord(ctx, mockPool, testTrans)
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusNotFound, result.ServiceErr.Status)
+
+		itemRow.AssertExpectations(t)
+		mockPool.AssertExpectations(t)
+	})
+
+	t.Run("Vendor Id mismatch", func(t *testing.T) {
+		mockPool := &it.MockPool{}
+		itemRow := &it.MockRow{}
+
+		testVid1 := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testVid2 := pgtype.UUID{Bytes: [16]byte{2}, Valid: true}
+		testBid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testTid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+		testTrans := repository.CreateTransactionParams{
+			Bid: testBid,
+			Vid: testVid1,
+			Iid: testIid,
+			Amt: pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+		}
+		testItem := repository.Item{
+			Iid:         testIid,
+			Vid:         testVid2,
+			Name:        "Schrodinger's Cat 1",
+			Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+			Description: utils.MakePointer("Probably dead"),
+			Cost:        pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+		}
+
+		it.SetupPoolQueryRow(mockPool, itemRow, repository.GetItemById, ctx, []any{testIid})
+		it.SetupMock(itemRow, "Scan", []any{mock.AnythingOfType("*pgtype.UUID")}, nil).Run(func(args mock.Arguments) {
+			if dest, ok := args.Get(0).(*pgtype.UUID); ok {
+				*dest = testTid
+			}
+		})
+		it.SetupMock(itemRow, "Scan", []any{
+			mock.AnythingOfType("*pgtype.UUID"),
+			mock.AnythingOfType("*pgtype.UUID"),
+			mock.AnythingOfType("*string"),
+			mock.AnythingOfType("**string"),
+			mock.AnythingOfType("**string"),
+			mock.AnythingOfType("*pgtype.Numeric")}, nil).Run(
+			func(args mock.Arguments) {
+				if dest, ok := args.Get(0).(*pgtype.UUID); ok {
+					*dest = testItem.Iid
+				}
+				if dest, ok := args.Get(1).(*pgtype.UUID); ok {
+					*dest = testItem.Vid
+				}
+				if dest, ok := args.Get(2).(*string); ok {
+					*dest = testItem.Name
+				}
+				if dest, ok := args.Get(3).(**string); ok {
+					*dest = testItem.Pictureurl
+				}
+				if dest, ok := args.Get(4).(**string); ok {
+					*dest = testItem.Description
+				}
+				if dest, ok := args.Get(5).(*pgtype.Numeric); ok {
+					*dest = testItem.Cost
+				}
+			},
+		)
+
+		result := CreateTransactionRecord(ctx, mockPool, testTrans)
+		if result.ServiceErr != nil {
+			it.PrintError(result.ServiceErr.Err, t)
+		}
+
+		assert.NotNil(t, result.ServiceErr)
+		assert.Error(t, result.ServiceErr.Err)
+		assert.Equal(t, http.StatusBadRequest, result.ServiceErr.Status)
+
+		mockPool.AssertExpectations(t)
+	})
+
+	// t.Run("Insufficent funds", func(t *testing.T) {
+	// 	mockPool := &it.MockPool{}
+	// 	itemRow := &it.MockRow{}
+	//
+	// 	testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testBid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testTid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testTrans := repository.CreateTransactionParams{
+	// 		Bid: testBid,
+	// 		Vid: testVid,
+	// 		Iid: testIid,
+	// 		Amt: pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+	// 	}
+	// 	testItem := repository.Item{
+	// 		Iid:         testIid,
+	// 		Vid:         testVid,
+	// 		Name:        "Schrodinger's Cat 1",
+	// 		Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+	// 		Description: utils.MakePointer("Probably dead"),
+	// 		Cost:        pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+	// 	}
+	//
+	// 	mockPool.AssertExpectations(t)
+	// })
+	//
+	// t.Run("CreateTransactionRecord error", func(t *testing.T) {
+	// 	mockPool := &it.MockPool{}
+	// 	itemRow := &it.MockRow{}
+	//
+	// 	testVid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testBid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testIid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testTid := pgtype.UUID{Bytes: [16]byte{1}, Valid: true}
+	// 	testTrans := repository.CreateTransactionParams{
+	// 		Bid: testBid,
+	// 		Vid: testVid,
+	// 		Iid: testIid,
+	// 		Amt: pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+	// 	}
+	// 	testItem := repository.Item{
+	// 		Iid:         testIid,
+	// 		Vid:         testVid,
+	// 		Name:        "Schrodinger's Cat 1",
+	// 		Pictureurl:  utils.MakePointer("https://example.com/cat.jpg"),
+	// 		Description: utils.MakePointer("Probably dead"),
+	// 		Cost:        pgtype.Numeric{Int: big.NewInt(100), Valid: true},
+	// 	}
+	//
+	// 	mockPool.AssertExpectations(t)
+	// })
 
 }
