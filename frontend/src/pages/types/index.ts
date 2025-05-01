c5241148d344002ab42f1d558073bc6ebc3ab601
iipenaky <99400110+iipenaky@users.noreply.github.com>
2025-05-01 23:50:19 +0000

    more documentation

--- frontend/src/pages/types/index.ts
+++ frontend/src/pages/types/index.ts
@@ -1,24 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+// Interface for the custom fetch utility with basic HTTP methods
 export interface IFetch {
-    get: (url: string) => Promise<any>;
-    post: (url: string, data: any) => Promise<any>;
-    put: (url: string, data: any) => Promise<any>;
-    delete: (url: string) => Promise<any>;
+    get: (url: string) => Promise<any>;           // Make a GET request to a URL
+    post: (url: string, data: any) => Promise<any>; // Make a POST request with data
+    put: (url: string, data: any) => Promise<any>;  // Make a PUT request to update data
+    delete: (url: string) => Promise<any>;         // Make a DELETE request to a URL
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
 
+// Structure of data required to sign up a user
 export interface SignupData {
     email: string;
     password: string;
     name: string;
-    isVendor: boolean;
+    isVendor: boolean; // true if the user is a vendor, false if buyer
 }
 
+// Structure of login credentials
 export interface LoginData {
     email: string;
     password: string;
 }
 
+// Expected response format after logging in
 export interface LoginResponse {
     data: {
         token: string;
@@ -29,6 +33,7 @@ export interface LoginResponse {
     };
 }
 
+// Expected response format after signing up
 export interface SignupResponse {
     data: {
         token: string;
@@ -39,19 +44,22 @@ export interface SignupResponse {
     };
 }
 
+// Structure for a marketplace item
 export type Item = {
-    iid: string;
-    vid: string;
+    iid: string;         // Item ID
+    vid: string;         // Vendor ID
     name: string;
     pictureurl: string;
     description: string;
     category: CATEGORIES;
-    quantity: number;
-    cost: number;
+    quantity: number;    // Available stock
+    cost: number;        // Price of item
 };
 
+// Structure for creating a new item (no ID or vendor yet)
 export type NewItem = Omit<Item, "iid" | "vid">;
 
+// Available item categories
 export enum CATEGORIES {
     FASHION = "FASHION",
     BOOKS_SUPPLIES = "BOOKS_SUPPLIES",
@@ -59,11 +67,13 @@ export enum CATEGORIES {
     ELECTRONICS = "ELECTRONICS",
 }
 
+// Types of users in the system
 export enum USER_TYPE {
     VENDOR = "vendor",
     BUYER = "buyer",
 }
 
+// Structure for a user object
 export type User = {
     token: string;
     uid: string;
@@ -72,20 +82,24 @@ export type User = {
     user_type: USER_TYPE;
 };
 
+// Standard structure for a successful response message
 export type ResponseMsg = {
     msg: string;
 };
 
+// Standard structure for an error message
 export type ResponseErr = {
     err: string;
 };
 
+// A record of a transaction (e.g., a purchase)
 export type Transaction = {
-    name: string;
-    amt: number;
-    t_time: string;
+    name: string;     // Buyer or item name
+    amt: number;      // Amount spent
+    t_time: string;   // Timestamp of transaction
 };
 
+// An item in a buyer's shopping cart
 export type CartItem = {
     vendor_name: string;
     name: string;
@@ -97,16 +111,19 @@ export type CartItem = {
     pictureurl: string;
 };
 
+// Item details along with vendor info for display
 export type ItemWithVendorInfo = Item & {
     vendor_name: string;
     vendor_logo: string;
     vendor_email: string;
 };
 
+// Type for an SVG icon component used in headers or UI
 export type Icon = React.FC<React.SVGProps<SVGSVGElement>>;
 
+// Structure for a navigation header item
 export type HeaderItem = {
-    name: string;
-    link: string;
-    icon?: Icon;
+    name: string;       // Label shown to users
+    link: string;       // Route or URL
+    icon?: Icon;        // Optional icon component
 };
