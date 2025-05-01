8a73b9e24ba59b44b141de624859a4be62ec6e03
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-05-01 13:16:53 +0000

    Add cart authorization middleware
    - Implement CartAuthMiddleware to limit cart access
    - Update cart routes to use new middleware
    - Rename cart route endpoints for clarity

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -43,3 +43,46 @@ func AuthMiddleware() gin.HandlerFunc {
 
 	}
 }
+
+func CartAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bidStr := c.Param("bId")
+		bid, err := utils.ParseUUID(bidStr)
+		logging.Infof("Bid str: %s", bidStr)
+		if err != nil {
+			utils.SendErrAbort(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		if authHeaders, ok := c.Request.Header["Authorization"]; ok {
+			logging.Infof("AuthHeaders: %v", authHeaders)
+			token, err := utils.ParseJWT(authHeaders[0], jwt.MapClaims{})
+
+			if err != nil {
+				utils.SendErrAbort(c, http.StatusInternalServerError, err)
+				return
+			}
+
+			uid, ok := (token.Claims).(jwt.MapClaims)["uid"]
+			if !ok {
+				utils.SendErrAbort(c, http.StatusBadRequest, errors.New("invalid token"))
+				return
+			}
+
+			uid, err = utils.ParseUUID(uid.(string))
+			if err != nil {
+				utils.SendErrAbort(c, http.StatusBadRequest, errors.New("invalid token"))
+				return
+			}
+
+			logging.Infof("Bid: %s\nUid: %s", bid, uid)
+			if uid != bid {
+				utils.SendErrAbort(c, http.StatusBadRequest, errors.New("cannot access other users' carts"))
+				return
+			}
+
+			c.Next()
+			return
+		}
+	}
+}
