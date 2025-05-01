3f57c10bedc3725b9c40921dcc0f8fd118bc79b6
iipenaky <99400110+iipenaky@users.noreply.github.com>
2025-05-01 23:20:51 +0000

    documented

--- backend/services/info.go
+++ backend/services/info.go
@@ -8,11 +8,19 @@ import (
 	"net/http"
 )
 
+// Health checks the database connection by fetching its version
 func Health(ctx context.Context, pool db.Pool) (string, *utils.ServiceError) {
+	// Create a new repository instance to interact with the database
 	q := repository.New(pool)
+
+	// Query the database to get the current database version
 	version, err := q.DbVersion(ctx)
+
+	// If there is an error fetching the database version, return an internal server error
 	if err != nil {
 		return "", &utils.ServiceError{Err: err, Status: http.StatusInternalServerError}
 	}
+
+	// Return the database version if successful, along with a nil error
 	return version, nil
 }
